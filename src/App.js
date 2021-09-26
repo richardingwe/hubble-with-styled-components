6e2b42a325402b74139a75e918ee0754501b977a
richardingwe <richardingwe5@gmail.com>
2021-09-26 11:02:40 +0100

    finished up

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import Header from './components/Header';
-import { Container } from "./components/styles/Container.styled";
+import { ThemeProvider } from 'styled-components'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Card from './components/Card'
+import { Container } from './components/styles/Container.styled'
+import GlobalStyles from './components/styles/Global'
+import content from './content'
+
+const theme = {
+  colors: {
+    header: '#ebfbff',
+    body: '#fff',
+    footer: '#003333',
+  },
+  mobile: '768px',
+}
 
 function App() {
   return (
-    <>
-      <Header />
-      <Container>
-        <h1>Hello World</h1>
-      </Container>
-    </>
-  );
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <Header />
+        <Container>
+          {content.map((item, index) => (
+            <Card key={index} item={item} />
+          ))}
+        </Container>
+        <Footer />
+      </>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
